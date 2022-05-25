bcee0e8e24df737a567389a73f6946d0e364baef
emmamkb <emmanuelmakabu7@gmail.com>
2022-05-25 23:53:11 +0200

    ✨ Create Dashboard home page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+// Dashboard Routes
+
+Route::get('dashboard', [HomeController::class, 'index'])->name('dashboard_home');
