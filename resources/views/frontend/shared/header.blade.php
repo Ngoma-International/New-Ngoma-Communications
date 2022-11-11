3d59cd7bb75b5387f9e5ad1adc4d98b701238968
scott Tresor <scotttresor@gmail.com>
2022-11-11 08:53:39 +0200

    fix (frontend) fix users frontend

--- resources/views/frontend/shared/header.blade.php
+++ resources/views/frontend/shared/header.blade.php
@@ -48,6 +48,38 @@
                 </div>
             </header>
         </div>
+        <section class="intro_section page_mainslider ds darkblue_gradient all-scr-cover">
+            <div class="flexslider" data-dots="true" data-nav="false">
+                <ul class="slides">
+                    <li>
+                        <div class="slide-image-wrap">
+                            <img src="{{ asset('assets/images/Triplex 3.svg') }}" alt="" />
+                        </div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <div class="slide_description_wrapper">
+                                        <div class="slide_description">
+                                            <div class="intro-layer to_animate" data-animation="fadeInUp">
+                                                <h1>Ngoma<span class="thin">Communications</span></h1>
+                                            </div>
+                                            <div class="intro-layer to_animate" data-animation="fadeInUp">
+                                                <p>We Inspire and Equip Individuals and Organisations to Progress and Develop to Their Full Potential.</p>
+                                            </div>
+                                            <div class="intro-layer to_animate" data-animation="fadeInUp">
+                                                <div class="slide_buttons">
+                                                    <a href="{{ route('seminar.index') }}" class="theme_button inverse min_width_button">View Ours Seminar</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
     @else
         @include('frontend.component.wrapper')
         <header class="page_header header_v2 header_darkgrey darkblue_gradient toggler_relative">
@@ -95,9 +127,5 @@
                 </div>
             </div>
         </header>
-    @endif
-
-    @if(Request::url() == route('app.name'))
-        @include('frontend.component.mainslider')
     @endif
 </div>
