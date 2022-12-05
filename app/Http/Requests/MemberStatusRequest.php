22be8de2ead055e73174944562c47147479d082c
scott Tresor <scotttresor@gmail.com>
2022-12-05 14:22:44 +0200

    fix(individual) review individual member

---
+++ app/Http/Requests/MemberStatusRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use App\Models\Member;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class MemberStatusRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'phone_number' => [
+                'required',
+                Rule::exists(Member::class, 'phone_number')
+            ],
+            'status' => [
+                'required',
+                'bool'
+            ]
+        ];
+    }
+}
