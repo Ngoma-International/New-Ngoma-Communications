e11d7ef8122eee323db42c9dde70f8a023cd93a6
scott Tresor <scotttresor@gmail.com>
2022-12-11 21:52:08 +0200

    fx( public event) public event add new content

--- resources/views/app/component/_card.blade.php
+++ resources/views/app/component/_card.blade.php
@@ -2,27 +2,27 @@
     <div class="card1">
         <div class="image">
             <a href="{{ route('seminar.show', $seminar->id) }}">
-                <img class="d-block" style="background-position: center;max-height: 100%;background-size: cover" src="{{ asset('storage/'. $seminar->images) }}" alt="">
+                <img class="d-block" style="background-position: center;max-height: 100%;background-size: cover" src="{{ $seminar->renderPoster() }}" alt="">
             </a>
         </div>
 
         <div class="card-block">
             <a href="{{ route('seminar.show', $seminar->id) }}">
-                <p class="name" style="font-size: 14px; font-weight: 600">
-                    {{ ucfirst($seminar->name) ?? "" }}
+                <p class="name" style="font-size: 12px; font-weight: 600">
+                    {{ ucfirst($seminar->title) ?? "" }}
                 </p>
             </a>
             <p>
-            <span style="font-size: 12px; display: flex; justify-content: space-between; justify-items: center; flex-shrink: revert">
-                <span style="font-size: 14px; font-weight: 600">
+            <span style="font-size: 12px; display: flex; justify-content: space-between; justify-items: center; flex-shrink: revert;  margin-bottom: 2%;">
+                <span style="font-size: 12px; font-weight: 600">
                     {{ $seminar->created_at->diffForHumans() ?? "" }}
                 </span>
-                <span style="font-size: 14px; font-weight: 600">
+                <span style="font-size: 12px; font-weight: 600">
                     $ {{ $seminar->prices ?? "" }}
                 </span>
             </span>
-            <span style="font-size: 12px; font-weight: 600">
-               {{ $seminar->country ?? "" }}
+            <span style="font-size: 12px; font-weight: 600;">
+               {{ $seminar->address->city ?? "" }}
             </span>
             </p>
         </div>
